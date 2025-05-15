704a065ebd90bd332f6ebf633460638b492bb8a8
nyuiop <gapin.sla@gmail.com>
2025-05-15 14:08:00 +0300

    Update macadress.bat

--- macadress.bat
+++ macadress.bat
@@ -132,6 +132,17 @@ taskkill /IM VSS* /F
 netsh interface ipv4 delete address "Loopback Pseudo-Interface 1"  addr=127.0.0.1 gateway=all
 netsh winsock reset catalog
 netsh interface tcp set global rss=disabled
+netsh interface ipv4 reset arpcache
+netsh interface ipv4 reset destinationcache
+netsh interface ipv4 reset neighbors
+
+netsh interface ipv6 reset arpcache
+netsh interface ipv6 reset destinationcache
+netsh interface ipv6 reset neighbors
+
+netsh interface ipv6 delete arpcache
+netsh interface ipv6 delete destinationcache
+netsh interface ipv6 delete neighbors
 ipconfig /flushdns
 
 goto loop
